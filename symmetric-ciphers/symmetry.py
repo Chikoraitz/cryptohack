be80e100ca602d0da55da44998ac55d7b6d2a53d
Chikoraitz <lb9400565@outlook.com>
2025-07-15 22:06:44 +0100

    Add solution to Symmetry

---
+++ symmetric-ciphers/symmetry.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import requests
+
+from simplejson.errors import JSONDecodeError
+
+
+CRYPTOHACK_CHALLENGE_ENDPOINT: str = "http://aes.cryptohack.org/symmetry"
+
+
+def __encrypt(plaintext: str, iv: str) -> str:
+  """Challenge endpoint that returns AES-encrypted ciphertext in OFB mode
+  """
+  resp = requests.get(f"{CRYPTOHACK_CHALLENGE_ENDPOINT}/encrypt/{plaintext}/{iv}/")
+  try:
+    if "error" in resp.json():
+      raise Exception(resp.json()["error"])
+
+    return resp.json()["ciphertext"]
+  except JSONDecodeError:
+    print("Malformed hex input in __encrypt()")
+
+
+def __encrypt_flag() -> str:
+  """Challenge endpoint that returns the flag plaintext DES-encrypted in ECB mode
+  """
+  resp = requests.get(f"{CRYPTOHACK_CHALLENGE_ENDPOINT}/encrypt_flag/")
+  return resp.json()["ciphertext"]
+
+
+def exploit(verbose: bool = True) -> str:
+  """
+  """
+  ciphertext: str = __encrypt_flag()
+  iv: str = ciphertext[:32]
+  plaintext_hex: str = __encrypt(ciphertext[32:], iv)
+
+  if verbose:
+    print(f"IV: {iv}")
+
+  return bytes.fromhex(plaintext_hex).decode("utf-8" )
+
+
+if __name__ == "__main__":
+  flag: str = challenge_solution(verbose=False)
+  print(f"Flag: {flag}")
